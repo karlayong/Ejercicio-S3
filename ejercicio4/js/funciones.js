f0ae67f1c533ae54c20dc1875aac0c7690728e9d
karla <karlayongzea@gmail.com>
2022-09-17 10:52:19 -0500

    agregando portada

--- ejercicio4/js/funciones.js
+++ ejercicio4/js/funciones.js
@@ -1,7 +1,7 @@
 /* Se pide ingresar el nombre de un producto y su precio. Calcular el igv (18% del producto). Mostrar el
 producto, el precio y el monto del igv*/
 
-let producto=prompt('Introduzca el nombre de prodcuto');
+let producto=prompt('Introduzca el nombre de producto');
 let precio=prompt('Introduzca el precio del producto');
 
 var igv = 0.18
